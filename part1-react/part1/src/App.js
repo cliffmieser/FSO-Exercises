1a5b5edc0f36ccd22d58f89cb85ff42ff6990d8d
cliffmieser <cliffmieser@gmail.com>
2023-02-09 00:20:45 -0500

    reset clicks counter component added

--- part1-react/part1/src/App.js
+++ part1-react/part1/src/App.js
@@ -1,45 +1,58 @@
 import { useState } from 'react'
 
 
-const Display = (props) => {
-  return(
-    <div>{props.counter}</div>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = (props) => {
-  return(
-    <button onClick={props.onClick}>
-       {props.text}
-    </button>
-  )
-}
+const Button = ({ onClick, text}) => <button onClick={onClick}>{text}</button>
+
+function App(){
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0);
+  const setZero = () =>{
+    //sets left and right properties to 0
+    //set array to []
+    setLeft(0);
+    setRight(0);
+    setAll([]);
+  }
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const handleLeftClick = () => {
+    //if allClicks length greater than or equal to 20
+    //set the array to []
+    if(allClicks.length >= 20){
+      setAll([]);
+    } else{ 
+      setAll(allClicks.concat('L'));
+      setLeft(left + 1);
+    }
+    
+  }
+
+  const handleRightClick = () => {
+    if(allClicks.length >= 20){
+      //if allClicks length greater than or equal to 20
+      //set the array to []
+      setAll([]);
+    } else{
+      setAll(allClicks.concat('R'));
+      setRight(right + 1);
+    }
+    
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button 
-        onClick={increaseByOne}
-        text='plus'
-        />
-        <Button 
-          onClick={setToZero}
-          text='zero' 
-          />
-        <Button 
-          onClick={decreaseByOne}
-          text='minus'
-          />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={setZero}>zero</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
 
-export default App
+export default App 
