5e925cfd6b5a96fb2392ee5c9ab1b743bb518900
cliffmieser <cliffmieser@gmail.com>
2023-02-09 22:54:51 -0500

    refactoring

--- part1-react/unicafe/src/App.js
+++ part1-react/unicafe/src/App.js
@@ -7,7 +7,12 @@ const Button = ({ handleRating, text }) => (
   </button>
 )
 
-//handle button coomponent
+//handle Statistics component
+const Statistics = ( {showStat, text} ) => {
+  return( 
+    <p><b>{text}:</b> {showStat}</p>
+  )
+}
 
 
 function App() {
@@ -15,22 +20,26 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
   //function definitions
   const handleGood = () => {
     //click on good increases count for good rating
+    setAll(allClicks.concat('good'));
     setGood(good + 1);
     console.log(`Good total: ${good}`);
   }
 
   const handleNeutral = () => {
-    //click on good increases count for good rating
+    //click on this increases count for this rating
+    setAll(allClicks.concat('neutral'));
     setNeutral(neutral + 1);
     console.log(`neutral total: ${neutral}`);
   }
 
   const handleBad = () => {
-    //click on good increases count for good rating
+    //click on this increases count for this rating
+    setAll(allClicks.concat('bad'));
     setBad(bad + 1);
     console.log(`bad total: ${bad}`);
   }
@@ -42,6 +51,9 @@ function App() {
       <Button handleRating={handleNeutral} text='neutral' />
       <Button handleRating={handleBad} text='bad' />
       <h3>Statistics</h3>
+      <Statistics showStat={good} text='good'/>
+      <Statistics showStat={neutral} text='neutral'/>
+      <Statistics showStat={bad}  text='bad'/>
 
       
     </div>
